e7c359d70e0a5128ccf093c9900dde27f877a73b
Kasper Furnes Bernhoff <kb@kfbi.xyz>
2019-01-15 09:34:58 +0100

    daily-git: Show contribution count

--- daily-git/README.md
+++ daily-git/README.md
@@ -4,7 +4,7 @@ I've become obsessed with my github stats, why not view it from the commandline!
 
 I'm well aware that node versions exists, but I avoid that when I can.
 
-- [ ] Show contribution count
+- [x] Show contribution count
 - [ ] Daily contribution stats*
 - *[ ] show calendar
 - [ ] Stable project warning, 0 contributions today warning
@@ -12,3 +12,4 @@ I'm well aware that node versions exists, but I avoid that when I can.
 - [ ] pie charts, idea taken from the node versin of git-stats
 - [ ] 8 bit version of profile picture
 - [ ] Stats for the current repo or the whole profile/org
+- [ ] Time travel mode, showing the commits flying by and the Calendar being filled with green!
--- daily-git/main.go
+++ daily-git/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
 func main() {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	client := githubv4.NewClient(httpClient)
+
+	var query struct {
+		Viewer struct {
+			ContributionsCollection struct {
+				TotalCommitContributions githubv4.Int
+			}
+			Login     githubv4.String
+			CreatedAt githubv4.DateTime
+		}
+	}
+	err := client.Query(context.Background(), &query, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(query.Viewer.Login)
+	fmt.Println("Joined", query.Viewer.CreatedAt.Format(time.RFC850))
+	fmt.Println(query.Viewer.ContributionsCollection.TotalCommitContributions, "Contributions in the last year")
 
 }
