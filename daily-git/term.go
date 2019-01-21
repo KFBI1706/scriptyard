578228ce50ed451ad4b9a3fdeda127304ee57cce
Kasper Furnes Bernhoff <kb@kfbi.xyz>
2019-01-22 00:39:20 +0100

    daily-git: Allign calendar

--- daily-git/term.go
+++ daily-git/term.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gdamore/tcell"
+	"github.com/y0ssar1an/q"
 )
 
 func termInit() {
@@ -47,17 +48,30 @@ func termInit() {
 	dur := time.Duration(0)
 loop:
 	for {
+		makeBorder(s)
 		makeCalendar(s)
 		select {
 		case <-quit:
 			break loop
-		case <-time.After(time.Second * 60):
+		case <-time.After(time.Second * 5):
 		}
 		start := time.Now()
 		dur += time.Now().Sub(start)
 	}
 
 	s.Fini()
+}
+
+func makeBorder(s tcell.Screen) {
+	w, h := s.Size()
+	st := tcell.StyleDefault
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			if i == 0 || j == 0 || i == w-1 || j == h-1 {
+				s.SetCell(i, j, st, '*')
+			}
+		}
+	}
 }
 
 func makeCalendar(s tcell.Screen) {
@@ -67,14 +81,15 @@ func makeCalendar(s tcell.Screen) {
 
 	for i := 0; i < w; i++ {
 		for j := 0; j < h; j++ {
-			if i == w-weekCnt-PAD || j == h-7-PAD || i == w-PAD || j == h-PAD {
-				s.SetCell(i, j, st, '*')
-			} else if i >= PAD && i < weekCnt+PAD && j >= PAD && j < 7+PAD {
-				color := tcell.ColorWhiteSmoke
-				if j-PAD < len(weeks[i-PAD].ContributionDays) {
-					color = tcell.GetColor(string(weeks[i-PAD].ContributionDays[j-PAD].Color))
+			if j < h-PAD && j > h-7-PAD {
+				if ii, jj := i, j-(h-7-PAD); ii >= PAD && ii < weekCnt+PAD && jj >= PAD && jj < 7+PAD {
+					q.Q(ii, jj) //DEBUG
+					color := tcell.ColorWhiteSmoke
+					if jj-PAD < len(weeks[ii-PAD].ContributionDays) {
+						color = tcell.GetColor(string(weeks[ii-PAD].ContributionDays[jj-PAD].Color))
+					}
+					s.SetCell(i, j, st.Background(tcell.ColorWhiteSmoke).Foreground(color), '■')
 				}
-				s.SetCell(i, j, st.Background(tcell.ColorWhiteSmoke).Foreground(color), '■')
 			}
 		}
 	}
