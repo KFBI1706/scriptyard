1ba7332569863d90e0406bb0be0d516f1e889e35
Kasper Furnes Bernhoff <kb@kfbi.xyz>
2019-05-09 03:48:16 +0200

    added nitro ignore and basic nitro app

--- budgetDiscordNitro/cmd/nitro/main.go
+++ budgetDiscordNitro/cmd/nitro/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	emojis []Emoji
+)
+
+// Emoji is a small struct with the basic information about an emoji
+type Emoji struct {
+	Name     string `json:"name"`
+	URL      string `json:"url"`
+	Animated bool   `json:"animated"`
+}
 
 func main() {
-	fmt.Println("vim-go")
+	res, err := http.Get("http://kfbi.xyz:11337/api/v1/emojis")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&emojis)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(emojis)
+
 }
